8e2c72b8395c57ca3a6c03c22cc0c14b4b9efea7
Rashoodbin <rashid.pvt420@gmail.com>
2026-04-01 22:36:59 +0400

    chore: remove build artifacts from version control

--- packages/shared/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/firebase';
-export * from './utils/uaePricingEngine';
-export * from './utils/RateLimiter';
-export * from './theme/binGroupTheme';
-export * from './context/LanguageContext';
--- packages/shared/dist/utils/propertyClassifier.d.ts
+++
@@ -1,2 +0,0 @@
-export type PropertyClassification = 'RESIDENTIAL' | 'COMMERCIAL' | 'INDUSTRIAL' | 'GOVERNMENT_MAJLIS' | 'GOVERNMENT_PROPERTY' | 'HOTEL' | 'MIXED_USE' | 'INSTITUTIONAL';
-export declare const classifyProperty: (inputs: any) => PropertyClassification;
