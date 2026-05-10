3ce1eb4aacc201e34089f99ce993d41c0ea3cc7e
Rashoodbin <rashid.pvt420@gmail.com>
2026-05-10 17:35:10 +0400

    Relax non-blocking lint rules for production pipeline

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,5 +12,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'prefer-const': 'off',
   },
 };
