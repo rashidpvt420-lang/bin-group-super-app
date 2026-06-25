d825ede39ee35939ec252b58db33750ebb6dbbac
Rashoodbin <rashid.pvt420@gmail.com>
2026-06-25 21:14:28 +0400

    chore(functions): expose rent ledger mirror

--- functions/runtimeAll.ts
+++ functions/runtimeAll.ts
@@ -1,3 +1,4 @@
 export * from './runtime';
 export * from './whatsappWebhook';
+export * from './rentLedgerMirror';
 export { onOwnerApprovalDecision, submitOwnerApprovalDecision } from './ownerTrustWorkflow';
