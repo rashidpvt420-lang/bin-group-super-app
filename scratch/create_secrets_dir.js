1e1b6e49c4796d916b64bc3cbf6c4eed63bbc7be
rashidpvt420 <58630094-rashidpvt420@users.noreply.replit.com>
2026-07-01 13:52:21 +0000

    Git commit prior to merge

--- scratch/create_secrets_dir.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = 'C:\\Users\\My-PC\\secrets';
-if (!fs.existsSync(path)) {
-    fs.mkdirSync(path, { recursive: true });
-    console.log(`Directory created: ${path}`);
-} else {
-    console.log(`Directory already exists: ${path}`);
-}
