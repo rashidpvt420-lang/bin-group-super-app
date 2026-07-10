0d37d5e8268e4b3ed6aff9a57c7675710db77f11
Rashoodbin <rashid.pvt420@gmail.com>
2026-07-10 14:22:15 +0400

    Compose canonical technician workforce center

---
+++ src/technician/pages/TechnicianWorkforceCenterPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TechnicianWorkforceOverview from '../components/TechnicianWorkforceOverview';
+import TechnicianHRPageV2 from './TechnicianHRPageV2';
+
+export default function TechnicianWorkforceCenterPage() {
+  return (
+    <>
+      <TechnicianWorkforceOverview />
+      <TechnicianHRPageV2 />
+    </>
+  );
+}
