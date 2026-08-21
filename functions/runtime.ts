aedc4ff1e17d3576ce349483904277edc8dbebcb
Rashoodbin <rashid.pvt420@gmail.com>
2026-08-21 08:32:43 +0400

    fix(admin): export protected staff lifecycle callables

--- functions/runtime.ts
+++ functions/runtime.ts
@@ -37,6 +37,7 @@ export * from "./ticketNormalization";
 export * from "./hrAutomation";
 export * from "./technicianPayrollCompatibility";
 export * from "./adminUserProvisioning";
+export * from "./adminStaffLifecycle";
 export * from "./adminBridgeAuth";
 export * from "./adminSecurityProfile";
 export * from "./adminMfaReadiness";
