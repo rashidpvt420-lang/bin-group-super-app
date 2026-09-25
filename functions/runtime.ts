2e2b9d5d3b8b6b3a4d01df86872ecf036c4e36ff
Rashoodbin <rashid.pvt420@gmail.com>
2026-09-25 12:48:56 +0400

    Phase 8: export Admin Broker KYC review summary

--- functions/runtime.ts
+++ functions/runtime.ts
@@ -112,7 +112,7 @@ export { adminAssignTechnician } from "./secureAdminTechnicianAssignment";
 export { tenantRequestUnitLink } from "./secureTenantUnitLinkRequest";
 export { adminResolveTenantUnitLink } from "./secureTenantUnitLinkOperations";
 export { submitBrokerKycProfile, getBrokerKycProfileSummary } from "./secureBrokerKycSubmission";
-export { adminReviewBrokerKyc } from "./secureBrokerKycReview";
+export { adminReviewBrokerKyc, getAdminBrokerKycReviewSummary } from "./secureBrokerKycReview";
 export { adminReviewBrokerPayoutRequest } from "./adminBrokerPayoutReview";
 export {
   requestBrokerPayoutOtp,
